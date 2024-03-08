661e06b12bea07686df31a009c5844bb50384823
JostTim <timothe.jost@wanadoo.fr>
2024-03-08 20:46:22 +0100

    check channels is list

--- exaspim/operations/waveform_generator.py
+++ exaspim/operations/waveform_generator.py
@@ -5,7 +5,17 @@ from scipy import interpolate
 
 
 def plot_waveforms_to_pdf(t, voltages_t):
-    etl_t, camera_enable_t, stage_enable_t, laser_488_enable_t, laser_638_enable_t, laser_561_enable_t, laser_405_enable_t, galvo_a_t, galvo_b_t = voltages_t  # TODO fix this, use AO names to channel number to autopopulate
+    (
+        etl_t,
+        camera_enable_t,
+        stage_enable_t,
+        laser_488_enable_t,
+        laser_638_enable_t,
+        laser_561_enable_t,
+        laser_405_enable_t,
+        galvo_a_t,
+        galvo_b_t,
+    ) = voltages_t  # TODO fix this, use AO names to channel number to autopopulate
 
     fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(10, 9))
     axes.set_title("One Frame.")
@@ -25,9 +35,12 @@ def plot_waveforms_to_pdf(t, voltages_t):
     fig.savefig("plot.pdf")
 
 
-def generate_waveforms(cfg, plot: bool = False, channels: list[int] = None, live = False):
+def generate_waveforms(cfg, plot: bool = False, channels: list[int] = None, live=False):
     voltages_t = {}
     total_samples = 0
+
+    if not isinstance(channels, list):
+        raise ValueError(f"Channels must be a list ! {channels}")
 
     # Create lookup table to go from ao channel name to voltages_t index.
     #   This must match the order the NI card will create them.
@@ -44,51 +57,103 @@ def generate_waveforms(cfg, plot: bool = False, channels: list[int] = None, live
         # Create channel-specific samples arrays for various relevant timings
         camera_delay_samples = int(cfg.daq_sample_rate * cfg.get_camera_delay_time(ch))
         etl_buffer_samples = int(cfg.daq_sample_rate * cfg.get_etl_buffer_time(ch))
-        channel_samples = camera_exposure_samples + etl_buffer_samples + rest_samples + dwell_time_samples
+        channel_samples = (
+            camera_exposure_samples
+            + etl_buffer_samples
+            + rest_samples
+            + dwell_time_samples
+        )
 
         total_samples += channel_samples
 
         voltages_t[ch] = np.zeros((len(cfg.n2c), channel_samples))
 
         # Generate ETL signal
-        t_etl = np.linspace(0, cfg.camera_exposure_time + cfg.get_etl_buffer_time(ch),
-                            camera_exposure_samples + etl_buffer_samples, endpoint=False)
+        t_etl = np.linspace(
+            0,
+            cfg.camera_exposure_time + cfg.get_etl_buffer_time(ch),
+            camera_exposure_samples + etl_buffer_samples,
+            endpoint=False,
+        )
         voltages_etl = -cfg.get_etl_amplitude(ch) * signal.sawtooth(
-            2 * np.pi / (cfg.camera_exposure_time + cfg.get_etl_buffer_time(ch)) * t_etl, width=1.0) + cfg.get_etl_offset(ch)
+            2
+            * np.pi
+            / (cfg.camera_exposure_time + cfg.get_etl_buffer_time(ch))
+            * t_etl,
+            width=1.0,
+        ) + cfg.get_etl_offset(ch)
         t0 = t_etl[0]
-        t1 = t_etl[int((camera_exposure_samples + etl_buffer_samples) * cfg.get_etl_interp_time(ch))]
+        t1 = t_etl[
+            int(
+                (camera_exposure_samples + etl_buffer_samples)
+                * cfg.get_etl_interp_time(ch)
+            )
+        ]
         tf = t_etl[-1]
         v0 = voltages_etl[0]
-        v1 = voltages_etl[int((camera_exposure_samples + etl_buffer_samples) * cfg.get_etl_interp_time(ch))] + cfg.get_etl_nonlinear(ch)
+        v1 = voltages_etl[
+            int(
+                (camera_exposure_samples + etl_buffer_samples)
+                * cfg.get_etl_interp_time(ch)
+            )
+        ] + cfg.get_etl_nonlinear(ch)
         vf = voltages_etl[-1]
-        f = interpolate.interp1d([t0, t1, tf], [v0, v1, vf], kind='quadratic')
+        f = interpolate.interp1d([t0, t1, tf], [v0, v1, vf], kind="quadratic")
         voltages_etl = f(t_etl)
 
-        voltages_t[ch][n2c_index['etl'], 0:camera_exposure_samples + etl_buffer_samples] = voltages_etl  # write in ETL sawtooth
-        voltages_t[ch][n2c_index['etl'], camera_exposure_samples + etl_buffer_samples::] = cfg.get_etl_offset(ch) + cfg.get_etl_amplitude(ch)  # snap back ETL after sawtooth
-        voltages_t[ch][n2c_index['etl'],
-        camera_exposure_samples + etl_buffer_samples:camera_exposure_samples + etl_buffer_samples + dwell_time_samples] = \
-            cfg.get_etl_offset(ch) - cfg.get_etl_amplitude(ch)  # delay snapback until last row is done exposing
+        voltages_t[ch][
+            n2c_index["etl"], 0 : camera_exposure_samples + etl_buffer_samples
+        ] = voltages_etl  # write in ETL sawtooth
+        voltages_t[ch][
+            n2c_index["etl"], camera_exposure_samples + etl_buffer_samples : :
+        ] = cfg.get_etl_offset(ch) + cfg.get_etl_amplitude(
+            ch
+        )  # snap back ETL after sawtooth
+        voltages_t[ch][
+            n2c_index["etl"],
+            camera_exposure_samples
+            + etl_buffer_samples : camera_exposure_samples
+            + etl_buffer_samples
+            + dwell_time_samples,
+        ] = cfg.get_etl_offset(ch) - cfg.get_etl_amplitude(
+            ch
+        )  # delay snapback until last row is done exposing
 
         # Generate camera TTL signal
-        voltages_t[ch][n2c_index['camera'], int(etl_buffer_samples / 2.0) + camera_delay_samples:int(
-            etl_buffer_samples / 2.0) + camera_delay_samples + pulse_samples] = 5.0
+        voltages_t[ch][
+            n2c_index["camera"],
+            int(etl_buffer_samples / 2.0)
+            + camera_delay_samples : int(etl_buffer_samples / 2.0)
+            + camera_delay_samples
+            + pulse_samples,
+        ] = 5.0
 
         # Generate laser TTL signal
-        voltages_t[ch][n2c_index[str(ch)],  # FIXME: remove n2c or move it into the config.
-        int(etl_buffer_samples / 2.0) + camera_delay_samples:int(
-            etl_buffer_samples / 2.0) + camera_exposure_samples + dwell_time_samples + camera_delay_samples] = cfg.get_channel_ao_voltage(str(ch))
+        voltages_t[ch][
+            n2c_index[str(ch)],  # FIXME: remove n2c or move it into the config.
+            int(etl_buffer_samples / 2.0)
+            + camera_delay_samples : int(etl_buffer_samples / 2.0)
+            + camera_exposure_samples
+            + dwell_time_samples
+            + camera_delay_samples,
+        ] = cfg.get_channel_ao_voltage(str(ch))
 
         # Generate stage TTL signal
         if ch == channels_list[-1]:
             volts = 5.0 if not live else 0.0
-            voltages_t[ch][n2c_index['stage'],
-                           camera_exposure_samples + etl_buffer_samples + dwell_time_samples:
-                           camera_exposure_samples + etl_buffer_samples + dwell_time_samples + pulse_samples] = volts
+            voltages_t[ch][
+                n2c_index["stage"],
+                camera_exposure_samples
+                + etl_buffer_samples
+                + dwell_time_samples : camera_exposure_samples
+                + etl_buffer_samples
+                + dwell_time_samples
+                + pulse_samples,
+            ] = volts
 
         # Generate galvo signals
-        voltages_t[ch][n2c_index['galvo_a']] = cfg.get_galvo_a_setpoint(ch)
-        voltages_t[ch][n2c_index['galvo_b']] = cfg.get_galvo_b_setpoint(ch)
+        voltages_t[ch][n2c_index["galvo_a"]] = cfg.get_galvo_a_setpoint(ch)
+        voltages_t[ch][n2c_index["galvo_b"]] = cfg.get_galvo_b_setpoint(ch)
 
     # Merge voltage arrays
     voltages_out = np.array([]).reshape((len(cfg.n2c), 0))
@@ -101,4 +166,3 @@ def generate_waveforms(cfg, plot: bool = False, channels: list[int] = None, live
         plot_waveforms_to_pdf(t, voltages_out)
 
     return voltages_out
-
